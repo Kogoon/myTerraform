147f1f00d1da8f555cfc2f16eba0f6ef0b73c49b
Kogoon <gojs712@gmail.com>
2022-07-05 17:38:23 +0900

    Digital Ocean Terraform

--- digitalocean/main.tf
+++ digitalocean/main.tf
@@ -2,16 +2,32 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      versoin = "~> 2.0"
+      version = "~> 2.0"
     }
   }
 }
 
-viriable "do_token" {}
+variable "do_token" {}
+
+data "digitalocean_ssh_key" "example" {
+  name = "do_workspace"
+}
 
 provider "digitalocean" {
   token = var.do_token
 }
 
 # Create a web server
+resource "digitalocean_droplet" "web" {
+  image     = "centos-7-x64"
+  name      = "web-1"
+  region    = "sfo3"
+  size      = "s-1vcpu-512mb-10gb"
+  ssh_keys  = [data.digitalocean_ssh_key.example.id]
+  user_data = file("user-data.sh")  
+}
+
+output "droplet_ip_address" {
+  value = digitalocean_droplet.web.ipv4_address
+}
 
