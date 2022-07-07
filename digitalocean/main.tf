3cebc74be904e14146a98996114baac71bc35045
Kogoon <gojs712@gmail.com>
2022-07-07 14:13:39 +0900

    Create Droplets using Terraform

--- digitalocean/main.tf
+++ digitalocean/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
-    }
-  }
-}
-
 variable "do_token" {}
 
 data "digitalocean_ssh_key" "example" {
@@ -20,14 +11,11 @@ provider "digitalocean" {
 # Create a web server
 resource "digitalocean_droplet" "web" {
   image     = "centos-7-x64"
-  name      = "web-1"
+  count     = 2
+  name      = "web-${count.index}"
   region    = "sfo3"
   size      = "s-1vcpu-512mb-10gb"
   ssh_keys  = [data.digitalocean_ssh_key.example.id]
   user_data = file("user-data.sh")  
-}
-
-output "droplet_ip_address" {
-  value = digitalocean_droplet.web.ipv4_address
 }
 
--- digitalocean/user-data.sh
+++ digitalocean/user-data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yum install -y httpd
 systemctl enable --now httpd
-export PUBLIC_IP=$(curl -s http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address)
+export PUBLIC_IP=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
 echo IP Address: $PUBLIC_IP > /var/www/html/index.html
