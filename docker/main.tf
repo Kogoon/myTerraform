523bb01817a5f22f8f714527761cb26929151098
Kogoon <gojs712@gmail.com>
2022-07-13 17:44:25 +0900

    Working on Docker with Terraform

--- docker/main.tf
+++ docker/main.tf
@@ -2,24 +2,42 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 2.15.0"
+      version = "2.17.0"
     }
   }
 }
 
+# Configure the docker provider
 provider "docker" {}
 
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
+# Create a docker image resource
+# -> docker pull nginx:latest
+#resource "docker_image" "myapp_image" {
+#  name         = "kogoon/test_commit:1.0"
+#  keep_locally = false
+#}
+
+data "docker_registry_image" "specific" {
+  name      = "kogoon/test_commit:1.0"
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
+resource "docker_image" "kogoon" {
+  name          = data.docker_registry_image.specific.name
+  pull_triggers = [data.docker_registry_image.specific.sha256_digest] 
+}
+
+# Create a docker container resource
+# -> same as 'docker run --name nginx -p 8080:80 -d nginx:latest'
+resource "docker_container" "myapp" {
+  image = docker_image.kogoon
   name  = var.container_name
   ports {
     internal = 80
     external = 8000
   }
 }
+
+# Or Create a service resource
+# -> same as 'docker service create -d -p 
+
 
