6fd902d49d2d2fcf6339d59525101514c6e9cb78
Kogoon <gojs712@gmail.com>
2022-07-15 11:02:50 +0900

    Study Kubernetes

--- digitalocean/main.tf
+++ digitalocean/main.tf
@@ -9,23 +9,25 @@ provider "digitalocean" {
 }
 
 # Create a web server
-resource "digitalocean_droplet" "web" {
+resource "digitalocean_droplet" "master" {
   image     = "centos-7-x64"
   count     = 1
-  name      = "web-${count.index}"
+  name      = "master-${count.index}"
   region    = "sfo3"
 #  size      = "s-1vcpu-512mb-10gb"
-  size      = "s-1vcpu-1gb"
+#  size      = "s-1vcpu-1gb"
+  size      = "s-2vcpu-2gb"
   ssh_keys  = [data.digitalocean_ssh_key.mykey.id]
   user_data = file("user-data.sh")  
 }
 
-#resource "digitalocean_droplet" "worker" {
-#  image     = "centos-7-x64"
-#  count     = 2
-#  name      = "docker-worker-${count.index}"
-#  region    = "sfo3"
+resource "digitalocean_droplet" "worker" {
+  image     = "centos-7-x64"
+  count     = 2
+  name      = "worker-${count.index}"
+  region    = "sfo3"
 #  size      = "s-1vcpu-512mb-10gb"
-#  ssh_keys  = [data.digitalocean_ssh_key.mykey.id]
-#  user_data = file("user-data.sh")
-#}
+  size      = "s-1vcpu-1gb"
+  ssh_keys  = [data.digitalocean_ssh_key.mykey.id]
+  user_data = file("user-data.sh")
+}
--- digitalocean/output.tf
+++ digitalocean/output.tf
@@ -1,16 +1,16 @@
 output "droplet_ip_address" {
   value = {
-    for droplet in digitalocean_droplet.web:
+    for droplet in digitalocean_droplet.master:
     droplet.name => droplet.ipv4_address
   }
 }
 
-#output "droplet_docker_ip_address" {
-#  value = {
-#    for droplet in digitalocean_droplet.docker-client:
-#    droplet.name => droplet.ipv4_address
-#  }
-#}
+output "droplet_docker_ip_address" {
+  value = {
+    for droplet in digitalocean_droplet.worker:
+    droplet.name => droplet.ipv4_address
+  }
+}
 
 #output "docker_worker_ip" {
 #  value = {
