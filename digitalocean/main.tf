d4cdd15392ff1a65ef240a8d1f20a372c7157c4d
Kogoon <gojs712@gmail.com>
2022-07-20 10:35:10 +0900

    Install K8S in user-data

--- digitalocean/main.tf
+++ digitalocean/main.tf
@@ -15,19 +15,19 @@ resource "digitalocean_droplet" "master" {
   name      = "master-${count.index}"
   region    = "sfo3"
 #  size      = "s-1vcpu-512mb-10gb"
-#  size      = "s-1vcpu-1gb"
-  size      = "s-2vcpu-2gb"
+  size      = "s-1vcpu-1gb"
+#  size      = "s-2vcpu-2gb"
   ssh_keys  = [data.digitalocean_ssh_key.mykey.id]
   user_data = file("user-data.sh")  
 }
 
-resource "digitalocean_droplet" "worker" {
-  image     = "centos-7-x64"
-  count     = 2
-  name      = "worker-${count.index}"
-  region    = "sfo3"
+#resource "digitalocean_droplet" "worker" {
+#  image     = "centos-7-x64"
+#  count     = 2
+#  name      = "worker-${count.index}"
+#  region    = "sfo3"
 #  size      = "s-1vcpu-512mb-10gb"
-  size      = "s-1vcpu-1gb"
-  ssh_keys  = [data.digitalocean_ssh_key.mykey.id]
-  user_data = file("user-data.sh")
-}
+#  size      = "s-1vcpu-1gb"
+#  ssh_keys  = [data.digitalocean_ssh_key.mykey.id]
+#  user_data = file("user-data.sh")
+#}
--- digitalocean/user-data.sh
+++ digitalocean/user-data.sh
@@ -4,3 +4,6 @@ yum install -y wget unzip net-tools telnet rdate
 rdate -s time.bora.net && clock -w
 curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker.sh
 systemctl start docker && systemctl enable docker
+cd /root/
+curl -LO https://dl.k8s.io/release/v1.22.2/bin/linux/amd64/kubectl
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
