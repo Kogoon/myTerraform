4738c4c90db6c6cdc86aafffdf828046b5c4ba8c
Kogoon <gojs712@gmail.com>
2022-07-22 17:51:47 +0900

    Install kubectl

--- digitalocean/output.tf
+++ digitalocean/output.tf
@@ -5,12 +5,12 @@ output "droplet_ip_address" {
   }
 }
 
-output "droplet_docker_ip_address" {
-  value = {
-    for droplet in digitalocean_droplet.worker:
-    droplet.name => droplet.ipv4_address
-  }
-}
+#output "droplet_docker_ip_address" {
+#  value = {
+#    for droplet in digitalocean_droplet.worker:
+#    droplet.name => droplet.ipv4_address
+#  }
+#}
 
 #output "docker_worker_ip" {
 #  value = {
--- digitalocean/user-data.sh
+++ digitalocean/user-data.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 curl -fsSL https://get.docker.com/ | sh
-yum install -y wget unzip net-tools telnet rdate
+yum install -y wget unzip net-tools telnet rdate git 
 rdate -s time.bora.net && clock -w
 curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker.sh
 systemctl start docker && systemctl enable docker
 cd /root/
 curl -LO https://dl.k8s.io/release/v1.22.2/bin/linux/amd64/kubectl
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+echo "alias k='kubectl'" >> /root/.bashrc
+systemctl disable --now firewalld
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=disabled/' /etc/selinux/config
+
